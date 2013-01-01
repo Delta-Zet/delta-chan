ea261321de6a4666d44def32ce36e7be3ab882fa
Alexander Maslov <drakmail@delta.pm>
2013-01-01 17:56:25 +0400

    Initial commmit with capistrano minimal config

---
+++ config/deploy.rb
@@ -0,0 +1,42 @@
+require 'bundler/capistrano'
+
+rails_env = ENV['RAILS_ENV'] || 'production'
+
+set :application, "delta-chan"
+set :rails_env, "production"
+set :domain, "web@chan.delta-zet.com"
+set :deploy_to, "/home/chan/#{application}"
+set :use_sudo, false
+set :repository,  "git@github.com:Delta-Zet/delta-chan.git"
+
+set :default_environment, {
+  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+}
+
+set :scm, :git
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+
+after 'deploy:update_code', :roles => :app do
+  run "rm -f #{current_release}/config/database.yml"
+  run "ln -s #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
+end
+
+namespace :deploy do
+  task :reset do
+    run "cd #{current_path}; rake db:reset RAILS_ENV=production"
+  end
+  task :restart do
+    run "thin -C #{current_release}/config/thin.yml restart"
+  end
+  task :start do
+    run "thin -C #{current_release}/config/thin.yml start"
+  end
+  task :stop do
+    run "thin -C #{current_release}/config/thin.yml stop"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeltaChan::Application.config.secret_token = 'c7ff7b13278bd32a696adc75a92df4b2047e2682ae20948c7d464fd23b19e6b3ea52fb7cca00ca601bd0d7a744d0538f80b4f4a6c79b96bd6eadfe637b88c927'
