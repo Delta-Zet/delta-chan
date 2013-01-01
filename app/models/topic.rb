40724e83bc65b75bdf0f4d8c7abbed82af9de7d2
Alexander Maslov <drakmail@delta.pm>
2013-01-01 22:57:41 +0400

    add some validations

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,7 @@ class Topic < ActiveRecord::Base
   belongs_to :section
   attr_accessible :author, :message, :title, :image, :section_id
   has_attached_file :image, :styles => { :thumb => "200x200>" }
+
+  validates :message, :title, :presence => true
+  validates :section_id, :numericality => { :only_integer => true }
 end
