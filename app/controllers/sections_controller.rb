df54c1546ab57774d9471d7de3889068831a45fd
Alexander Maslov <drakmail@delta.pm>
2013-01-01 23:10:09 +0400

    fix topics display

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -3,6 +3,6 @@ class SectionsController < ApplicationController
   def show
     @section = Section.find_by_slug(params[:id])
     @topic = Topic.new(section_id: @section.id)
-    @topics = Topic.all
+    @topics = Topic.find_all_by_section_id(@section.id)
   end
 end
