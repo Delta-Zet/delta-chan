1dcdc6b85be6b404c40be661f154c6836d2fe761
Alexander Maslov <drakmail@delta.pm>
2013-01-01 21:36:18 +0400

    fix section creation bug

--- app/controllers/admin/sections_controller.rb
+++ app/controllers/admin/sections_controller.rb
@@ -1,3 +1,13 @@
 class Admin::SectionsController < Admin::DashboardController
   crudify :section
+
+  def create
+    @section = Section.new(params[:section])
+    if @section.save
+      flash[:notice] = "Section created"
+      redirect_to admin_sections_path
+    else
+      render 'new'
+    end
+  end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -2,4 +2,5 @@ class Section < ActiveRecord::Base
   attr_accessible :description, :position, :slug, :title
 
   validates :description, :slug, :title, :presence => true
+  validates :position, :numericality => { :only_integer => true }
 end
